1e0f3ccf7f68703b75f46f977ba904320fca6ffa
Masterdark89 <126896860+Masterdark89@users.noreply.github.com>
2026-01-13 16:09:15 +0100

    Add files via upload
    Primeras funciones de creación de minas

--- BuscaMinas.py
+++ BuscaMinas.py
@@ -1,5 +1,39 @@
-# 3 dificultades 6x6 / 12x12 / 26x26
+# 3 dificultades 6x6 (6) / 12x12 (50) / 26x26 (200)
+import random
+
 seguir = True
+minas = 0
+tablero = []
+
+
+def coordenada():
+    while True:
+        coordenadas = input("Introduce la casilla")
+        if len(coordenadas) == columna:
+            return coordenadas
+        else:
+            print("Coordenadas no válidas. Intenta de nuevo")
+
+
+def crear_tablero(minas, columna):
+    # crea X minas en posiciones x e y
+
+    for i in range(minas):
+        posicion_mina_x = random.randint(0, columna)
+        posicion_mina_y = random.randint(0, columna)
+
+        if tablero(posicion_mina_x, posicion_mina_y) == 0:
+            tablero(posicion_mina_x, posicion_mina_y) = "*"
+        else:
+            minas = minas + 1
+
+
+def comprobar_minas():
+    # si la mina es x e y necesitamos sumar alrededor
+    # (x,y-1 x,y+1) (x-1,y-1 x-1,y x-1,y+1) (x+1,y-1 x+1,y x+1,y+1)
+    if posicion == "*":
+        # si encuentra una mina, sus posiciones alrededor + 1
+
 
 while (seguir):
     print("---------------------------")
@@ -18,41 +52,19 @@ while (seguir):
 
     match(opcion):
         case 1:
-            print("   " + "A" + " " + "B" + " " + "C" +
-                  " " + "D" + " " + "E" + " " + "F")
+            minas = 6
+            columna = 6
+            print("  ", end=" ")
+
+            for i in range(5):
+                print(chr(65 + i), end=" ")
+
+            print(chr(65 + 6) + " ")
 
             for i in range(1, 6):
                 print(i, "⬛️" * 6)
 
-        case 2:
-            print("   ", end=" ")
-
-            for i in range(25):
-                print(chr(65 + i), end=" ")
-
-            print(chr(65 + 25) + " ")
-
-            for i in range(1, 27):
-                if i < 10:
-                    print(f"0{i} {'⬛️' * 26}")
-
-                if i >= 10:
-                    print(i, "⬛️" * 26)
-
-        case 3:
-            print("   ", end=" ")
-
-            for i in range(11):
-                print(chr(65 + i), end=" ")
-
-            print(chr(65 + 12) + " ")
-
-            for i in range(1, 13):
-                if i < 10:
-                    print(f"0{i} {'⬛️' * 12}")
-
-                if i >= 10:
-                    print(i, "⬛️" * 12)
+            crear_tablero(minas, columna)
 
         case 4:
             print("Saliendo del programa...")
